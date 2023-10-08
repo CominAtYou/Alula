dc52322d40367a49e66a31112f4959d5bc47c379
William M. <cominatyou@cominatyou.com>
2023-10-08 02:57:09 -0500

    Add additional field to attachment cache index

--- src/db/mongoInstance.ts
+++ src/db/mongoInstance.ts
@@ -16,8 +16,8 @@ export async function createMongoConnection() {
         if (!(await mongoClient.db("main").listCollections({ name: "attachment_link_cache" }).hasNext())) {
             const collection = await mongoClient.db("main").createCollection("attachment_link_cache");
 
-            await collection.createIndex({ expireAt: 1 }, { expireAfterSeconds: 0 })
-            await collection.createIndex({ channelId: 1, messageId: 1, attachmentSnowflake: 1, filename: 1 });
+            await collection.createIndex({ expireAt: 1 }, { expireAfterSeconds: 0 });
+            await collection.createIndex({ channelId: 1, messageId: 1, attachmentSnowflake: 1, filename: 1, attachmentLink: 1 });
         }
 
         console.log("Connected to MongoDB!");
