dced14c1a1e74ddf623d8dfd11b6e40f4d1a336a
William M. <cominatyou@cominatyou.com>
2023-12-05 23:24:37 -0600

    Add ID for clear slash command

--- src/slashcommands/slashCommandRouter.ts
+++ src/slashcommands/slashCommandRouter.ts
@@ -2,11 +2,13 @@ import { CommandInteraction } from "discord.js";
 import anonymousSlashCommand from "./anonymous";
 import disableModmail from "./disableModmail";
 import closeThreadSlashCommand from "./closeThread";
+import clearUserEntry from "./clear";
 
 const slashCommands = {
     "1158087318431867023": anonymousSlashCommand,
     "1158611799088828518": disableModmail,
-    "1158860642669830185": closeThreadSlashCommand
+    "1158860642669830185": closeThreadSlashCommand,
+    "1181828360511438920": clearUserEntry
 }
 
 export default async function slashCommandRouter(interaction: CommandInteraction) {
