eacc8a33f09004535cb4d8e223b282e16c384513
William M. <cominatyou@cominatyou.com>
2024-03-22 20:34:47 -0500

    Use upsert instead of if check

--- src/slashcommands/disableModmail.ts
+++ src/slashcommands/disableModmail.ts
@@ -3,15 +3,9 @@ import { mongoDatabase } from "../db/mongoInstance";
 import GuildConfig from "../types/GuildConfig";
 
 export default async function disableModmail(interaction: ChatInputCommandInteraction) {
-    const newState = interaction.options.getBoolean("disabled");
+    const newState = interaction.options.getBoolean("disabled", true);
     const config = mongoDatabase.collection<GuildConfig>(`guildconfigs`);
 
-    if (await config.findOne({ guildId: interaction.guildId })) {
-        await config.updateOne({ guildId: interaction.guildId }, { $set: { modmailDisabled: newState } });
-        await interaction.reply(`New modmail threads have been ${newState ? "disabled" : "enabled"}.`);
-    }
-    else {
-        await config.insertOne({ guildId: interaction.guildId, modmailDisabled: newState });
-        await interaction.reply(`New modmail threads have been ${newState ? "disabled" : "enabled"}.`);
-    }
+    await config.updateOne({ guildId: interaction.guildId }, { $set: { modmailDisabled: newState } }, { upsert: true });
+    await interaction.reply(`New modmail threads have been ${newState ? "disabled" : "enabled"}.`);
 }
