0bac2808c3f5c29f9511bbbdf55a0ab8591aa588
William M. <cominatyou@cominatyou.com>
2024-06-09 19:36:28 -0500

    Exclude appeals channel from inactivity task

--- src/threads/threadExpiryTask.ts
+++ src/threads/threadExpiryTask.ts
@@ -3,6 +3,7 @@ import { mongoDatabase } from "../db/mongoInstance";
 import ActiveThread from "../types/ActiveThread";
 import closeThread from "./closeThread";
 import { scheduleJob } from "node-schedule";
+import { APPEALS_FORUM_CHANNEL_ID } from "../constants";
 
 export default function scheduleThreadExpiryTask(client: Client) {
     scheduleJob('0 0 * * *', async () => {
@@ -11,6 +12,10 @@ export default function scheduleThreadExpiryTask(client: Client) {
         for await (const doc of threads) {
             const thread = await client.channels.fetch(doc.receivingThreadId) as ThreadChannel;
             const message = await thread.messages.fetch(thread.lastMessageId);
+
+            if (thread.parentId === APPEALS_FORUM_CHANNEL_ID) {
+                continue;
+            }
 
             // one week expiry
             if (Date.now() - message.createdTimestamp > 604800000) {
