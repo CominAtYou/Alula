37a0603b48dba8cd9566dbf2b6111bc0a70049d1
William M. <cominatyou@cominatyou.com>
2024-07-10 01:24:35 -0500

    Lower April Fools chance from 0.05% to 0.01%

--- src/misc/aprilFools.ts
+++ src/misc/aprilFools.ts
@@ -3,7 +3,7 @@ import { Message } from "discord.js";
 export default function aprilFools(message: Message) {
     if (message.channel.id !== "222106327210655745") return;
 
-    if (Math.random() < 0.0005) {
+    if (Math.random() < 0.0001) {
         message.react("❌");
     }
 }
