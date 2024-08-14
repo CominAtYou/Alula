b6ee6faf42590082a7ba52458a9b40289af16955
William M. <cominatyou@cominatyou.com>
2024-08-14 16:06:12 -0500

    Add log event for submissions being toggled

--- src/slashcommands/disableModmail.ts
+++ src/slashcommands/disableModmail.ts
@@ -1,6 +1,7 @@
-import { ChatInputCommandInteraction } from "discord.js";
+import { ChatInputCommandInteraction, GuildTextBasedChannel } from "discord.js";
 import { mongoDatabase } from "../db/mongoInstance";
 import GuildConfig from "../types/GuildConfig";
+import { DEBUG_LOG_CHANNEL_ID } from "../constants";
 
 export default async function disableModmail(interaction: ChatInputCommandInteraction) {
     const newState = interaction.options.getBoolean("disabled", true);
@@ -8,4 +9,7 @@ export default async function disableModmail(interaction: ChatInputCommandIntera
 
     await config.updateOne({ guildId: interaction.guildId }, { $set: { modmailDisabled: newState } }, { upsert: true });
     await interaction.reply(`New modmail threads have been ${newState ? "disabled" : "enabled"}.`);
+
+    const logChannel = (interaction.client.channels.cache.get(DEBUG_LOG_CHANNEL_ID) ?? await interaction.client.channels.fetch(DEBUG_LOG_CHANNEL_ID)) as GuildTextBasedChannel;
+    await logChannel.send(`New modmail submissions have been ${newState ? "disabled" : "enabled"} by ${interaction.user.username}.`);
 }
