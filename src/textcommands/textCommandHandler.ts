bacbdadcd7b9d37b047faae12b5b726990021127
William M. <cominatyou@cominatyou.com>
2024-10-25 21:39:09 -0500

    Add archiveThread command

--- src/textcommands/textCommandHandler.ts
+++ src/textcommands/textCommandHandler.ts
@@ -1,9 +1,11 @@
 import { Message } from "discord.js";
 import setStatus from "./internal/status";
 import { TEXT_COMMAND_PREFIX } from "../constants";
+import archiveThread from "./internal/archiveThread";
 
 const commands = {
-    "status": setStatus
+    "status": setStatus,
+    "archive": archiveThread
 }
 
 export default function textCommandHandler(message: Message) {
